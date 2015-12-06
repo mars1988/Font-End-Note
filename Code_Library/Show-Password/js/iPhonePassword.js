05772a60f0a2bf14417caada69ce699eab8204f9
mxu <mxu@squiz.net>
2015-12-07 10:27:15 +1100

    新的代码库

---
+++ Code_Library/Show-Password/js/iPhonePassword.js
@@ -0,0 +1,10 @@
+$(function() {
+    // $("#user-password").keypress(function(e) {
+    //     $("#letterViewer")
+    //         .html(String.fromCharCode(e.which))
+    //         .fadeIn(100, function() {
+    //             $(this).fadeOut(100);
+    //         });     
+    // });
+   $('#user-password').dPassword();
+});
